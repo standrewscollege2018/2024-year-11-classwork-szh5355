2d67cf51830c6ab5e8720fde1455749898ebc304
szh5355 <szh5355@stacmail.net>
2024-02-08 15:17:08 +1300

    Added loop

--- double number.py
+++ double number.py
@@ -1,8 +1,12 @@
 '''This program takes a number as input and doubles it'''
 
-#Take number input as a float and then print double the number
-try:
-    num = float(input())
-    print(num*2)
-except ValueError:
-    print("Please enter a number")
+keep_asking = True
+while keep_asking == True 
+    #Take number input as a float and then print double the number
+    try:
+        num = float(input())
+        keep_asking = False
+    except ValueError:
+        print("Please enter a number")
+
+print(num*2)
