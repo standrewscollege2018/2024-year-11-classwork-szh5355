83720b7fe4577bf92b8de13ebabb4661023e5dbe
szh5355 <szh5355@stacmail.net>
2024-02-09 10:21:30 +1300

    Checking for positive and negative numbers

--- double number.py
+++ double number.py
@@ -1,12 +1,17 @@
 '''This program takes a number as input and doubles it'''
 
+#Start a loop for error catching
 keep_asking = True
-while keep_asking == True 
+while keep_asking:
     #Take number input as a float and then print double the number
     try:
-        num = float(input())
-        keep_asking = False
+        num = float(input("Enter a positive number:"))
+        if num >= 0:
+            keep_asking = False
+        else:
+            print("Enter a positive number:")
+            
     except ValueError:
         print("Please enter a number")
 
-print(num*2)
+print(f"{num} doubled is {num * 2}")
