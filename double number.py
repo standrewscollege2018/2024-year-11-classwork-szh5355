77dd6a4676ce916db8fa8a1963d6c22fd7bc77bc
szh5355 <szh5355@stacmail.net>
2024-02-08 15:05:46 +1300

    Trying whether the value is string or float

--- double number.py
+++ double number.py
@@ -1,2 +1,8 @@
-num = int (input())
-print (num*2)
+'''This program takes a number as input and doubles it'''
+
+#Take number input as a float and then print double the number
+try:
+    num = float(input())
+    print(num*2)
+except ValueError:
+    print("Please enter a number")
