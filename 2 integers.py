52e91246ac3f03a7090dbd6553d351fbfb64fa9b
szh5355 <szh5355@stacmail.net>
2024-02-12 09:23:37 +1300

    2 integers

---
+++ 2 integers.py
@@ -0,0 +1,12 @@
+'''This program demonstrates how a function works'''
+
+'''multiply() multiplies two numbers and returns the result'''
+def multiply(a, b):
+    return a * b
+
+#Get two numbers from the user
+num1=float(input("Enter a number: "))
+num2=float(input("Enter another number: "))
+
+#Display the product of those two numbers
+print(multiply(num1, num2))
--- blood_donor.py
+++ blood_donor.py
@@ -1,6 +1,34 @@
-age=int(input("Age:"))
-weight=int(input("Weight:"))
+def check(a):
+    try:
+        a = int(a)
+        return True
+    except ValueError:
+        return False
+
+get_age = True
+while get_age:
+    age=input("Age:")
+    if check(age):
+        get_age = False
+    else:
+        print("Oops")
+
+
+
+get_weight = True
+while get_weight:
+    weight=input("Weight:")
+    if check(weight):
+        get_weight = False
+    else:
+        print("Oops")
+
+
+
 if age >= 16 and weight >= 50:
     print ("You are eligible")
 else:
     print ("You are not eligible")
+
+
+    
