aff03926d9206f4d0e26bcf4e73a37e55474e11c
szh5355 <szh5355@stacmail.net>
2024-03-05 12:57:27 +1300

    raffle task

--- The raffle .py
+++ The raffle .py
@@ -1,9 +1,18 @@
+
+import random
 print("Welcome to the raffle program.")
 prize = input("What is the prize being raffled?")
 value = input("What is the value of the prize (do not enter $ sign)")
 
-name = input("Enter name of entrant: ")
-if name == 'end':
-    keep_going = False
-else:
-    keep_going = True
+names = []
+keep_asking = True
+while keep_asking ==True:
+    name = input("Enter name of entrant:")
+    if name == "End":
+        keep_asking = False
+    else:
+        names.append(name)
+
+number_of_entrants = len(names)
+winner = random.randint(0,number_of_entrants-1)
+print(names[winner])
